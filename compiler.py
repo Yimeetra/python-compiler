d467c62a9aac4932514bba9f39037653c8a8ac99
Yimeetra <0103art@gmail.com>
2025-09-21 16:55:04 +0200

    Add type tracking

--- compiler.py
+++ compiler.py
@@ -1,8 +1,37 @@
 import dis
 from types import CodeType
+from typing import Any
 import more_itertools
 import itertools
 import io
+from enum import Enum, auto
+from dataclasses import dataclass
+
+
+DEBUG = True
+
+
+class BuiltinTypesEnum(Enum):
+    UNKNOWN = auto()
+
+    NONE = auto()
+    INT = auto()
+    STRING = auto()
+
+    FUNCTION = auto()
+    NEXT_FUNCTION = auto()
+    
+    RANGE = auto()
+
+@dataclass(unsafe_hash=True)
+class Type:
+    id: int
+    name: str
+    payload: Any = None
+
+    def from_builtin(builtin: BuiltinTypesEnum, payload = None):
+        return Type(builtin.value, builtin.name, payload)
+
 
 cmp_op_to_suffix = {
     ">": "g",
@@ -22,6 +51,10 @@ bin_op_to_inst = {
 }
 
 args_to_regs_map = ["rdi", "rsi", "rdx", "rcx", "r8", "r9"]
+builtin_functions: dict[str, Type] = {
+    "print": Type.from_builtin(BuiltinTypesEnum.NONE),
+    "print_string": Type.from_builtin(BuiltinTypesEnum.NONE),
+}
 
 class Compiler:
     def __init__(self, input_file_name: str, output_file_name: str) -> None:
@@ -31,6 +64,8 @@ class Compiler:
         self._label_generator = self._get_next_label()
         self.output_file = open(output_file_name, "w+")
         self.codes_asm: dict[str, str] = {}
+        self.types: set = {Type(BuiltinTypesEnum(i).value, BuiltinTypesEnum(i).name) 
+                           for i in BuiltinTypesEnum._value2member_map_}
     
     def __del__(self):
         self.output_file.close()
@@ -66,14 +101,23 @@ class Compiler:
         
         f.write("section .data\n")
 
+        f.write("global None\n")
+        f.write("None: dq 0\n")
+
         for code in code_obj.co_consts:
             if isinstance(code, CodeType):
                 for i, const in enumerate(code.co_consts):
                     if isinstance(const, str):
                         f.write(f"{code.co_name}_CONST{i}: ")
-                        f.write(f"db \"{const.replace("\n", "\", 0xA, 0xD, \"")}\", 0\n")
+                        f.write('db "'+const.replace("\n", "\", 0xA, 0xD, \"")+'", 0\n')
 
     def compile_code(self, code_obj: CodeType) -> None:
+        types_stacks: list[list[Type]] = [[]]
+        variables_types: dict[str, Type] = {}
+
+        for var in code_obj.co_varnames:
+            variables_types[var] = Type.from_builtin(BuiltinTypesEnum.UNKNOWN)
+
         f = io.StringIO()
         jump_labels = {}
         instructions = more_itertools.seekable(dis.get_instructions(code_obj))
@@ -97,91 +141,122 @@ class Compiler:
             label = jump_labels.get(inst.offset)
             if label:
                 f.write(f"{label}:\n")
-            match inst.opname:
-                case "STORE_NAME":
-                    # TODO
-                    pass
-                case "RESUME":
-                    pass
-                case "LOAD_CONST":
-                    f.write(f"; {inst.offset} LOAD_CONST\n")
-                    match code_obj.co_consts[inst.arg]:
-                        case None:
-                            f.write("    push qword 0\n")
-                        case str():
-                            f.write(f"    lea rax, {code_obj.co_name}_CONST{inst.arg}\n")
-                            f.write("    push rax\n")
-                        case CodeType():
-                            if next(instructions).opname == "MAKE_FUNCTION" and instructions.peek().opname != "STORE_NAME":
-                                f.write(f"    lea rax, {code_obj.co_names[inst.arg-1]}\n")
-                        case int():
-                            f.write(f"    push qword {inst.argval}\n")
-                        case _:
-                            raise Exception(f"Constant type \"{type(inst.argval)}\" is unimplemented")
-                case "STORE_FAST":
-                    f.write(f"; {inst.offset} STORE_FAST\n")
-                    f.write("    pop rax\n")
-                    f.write(f"    mov [rbp-8*{inst.arg+1}], rax\n")
-                case "LOAD_GLOBAL":
-                    f.write(f"; {inst.offset} LOAD_GLOBAL\n")
-                    f.write(f"    lea rax, {code_obj.co_names[inst.arg-1]}\n")
-                    f.write("    push rax\n")
-                case "LOAD_FAST":
-                    f.write(f"; {inst.offset} LOAD_FAST\n")
-                    f.write(f"    push qword [rbp-8*{inst.arg+1}]\n")
-                case "BINARY_OP":
-                    f.write(f"; {inst.offset} BINARY_OP\n")
-                    f.write("    mov rax, [rsp+8]\n")
-                    match inst.argrepr:
-                        case "+"|"-"|"+="|"-="|"&":
-                            f.write(f"    {bin_op_to_inst[inst.argrepr]} rax, [rsp]\n")
-                        case "*":
-                            f.write("    imul rax, [rsp]\n")
-                        case "/":
-                            f.write("    cqo\n")
-                            f.write("    mov rcx, [rsp]\n")
-                            f.write("    idiv rcx\n")
-                        case _:
-                            raise Exception(f"Instruction {inst.opname}({inst.argrepr}) is not implemented")
-                    f.write("    add rsp, 16\n")
-                    f.write("    push rax\n")
-                case "CALL":
-                    f.write(f"; {inst.offset} CALL\n")
-                    for i in range(inst.arg):
-                        f.write(f"    pop {args_to_regs_map[i]}\n")
-                    f.write("    pop rax\n")
-                    f.write("    call rax\n")
-                    f.write("    push rax\n")
-                case "POP_TOP":
-                    f.write(f"; {inst.offset} POP_TOP\n")
-                    f.write("    add rsp, 8\n")
-                case "RETURN_CONST":
-                    f.write(f"; {inst.offset} RETURN_CONST\n")
-                    f.write(f"    add rsp, {(len(code_obj.co_varnames))*8}\n")
-                    f.write("    pop rbp\n")
-                    f.write(f"    mov rax, {0 if inst.argval is None else inst.argval}\n")
-                    f.write("    ret\n")
-                case "COMPARE_OP":
-                    f.write(f"; {inst.offset} COMPARE_OP\n")
-                    f.write("    mov rax, [rsp+8]\n")
-                    f.write("    cmp rax, [rsp]\n")
-                    f.write("    mov rax, 0\n")
-                    f.write(f"    set{cmp_op_to_suffix[inst.argval]} al\n")
-                    f.write("    add rsp, 16\n")
-                    f.write("    push rax\n")
-                case "POP_JUMP_IF_FALSE":
-                    f.write(f"; {inst.offset} POP_JUMP_IF_FALSE({jump_labels[inst.argval]})\n")
-                    f.write("    pop rax\n")
-                    f.write("    test rax, rax\n")
-                    f.write(f"    jz {jump_labels[inst.argval]}\n")
-                case "JUMP_BACKWARD":
-                    f.write(f"; {inst.offset} JUMP_BACKWARD({jump_labels[inst.argval]})\n")
-                    f.write(f"    jmp {jump_labels[inst.argval]}\n")
-                case "JUMP_FORWARD":
-                    f.write(f"; {inst.offset} JUMP_FORWARD({jump_labels[inst.argval]})\n")
-                    f.write(f"    jmp {jump_labels[inst.argval]}\n")
-                case _:
-                    raise Exception(f"Instruction {inst.opname}({inst.argrepr}) is not implemented")
+            try:
+                if DEBUG:
+                    if "JUMP" in inst.opname:
+                        f.write(f"; {inst.offset} {inst.opname}({jump_labels[inst.argval]})\n")
+                    else:
+                        f.write(f"; {inst.offset} {inst.opname}({inst.argrepr})\n")
+
+                if DEBUG:
+                    for stack in types_stacks:
+                        f.write("; TYPE_STACK: " + " -> ".join([i.name for i in stack]) + "\n")
+            
+                match inst.opname:
+                    case "STORE_NAME":
+                        # TODO
+                        pass
+                    case "RESUME":
+                        pass
+                    case "LOAD_CONST":
+                        match code_obj.co_consts[inst.arg]:
+                            case None:
+                                f.write("    lea rax, [None]\n")
+                                f.write("    push rax\n")
+                                types_stacks[-1].append(Type.from_builtin(BuiltinTypesEnum.NONE))
+                            case str():
+                                f.write(f"    lea rax, {code_obj.co_name}_CONST{inst.arg}\n")
+                                f.write("    push rax\n")
+                                types_stacks[-1].append(Type.from_builtin(BuiltinTypesEnum.STRING))
+                            case CodeType():
+                                if next(instructions).opname == "MAKE_FUNCTION" and instructions.peek().opname != "STORE_NAME":
+                                    f.write(f"    lea rax, {code_obj.co_names[inst.arg//2]}\n")
+                                    f.write("    push rax\n")
+                                    types_stacks[-1].append(Type.from_builtin(BuiltinTypesEnum.FUNCTION))
+                            case int():
+                                f.write(f"    push qword {inst.argval}\n")
+                                types_stacks[-1].append(Type.from_builtin(BuiltinTypesEnum.INT))
+                            case _:
+                                raise Exception(f"Constant type \"{type(inst.argval)}\" is unimplemented")
+                    case "STORE_FAST":
+                        f.write("    pop rax\n")
+                        f.write(f"    mov [rbp-8*{inst.arg+1}], rax\n")
+                        variables_types[inst.argval] = types_stacks[-1].pop()
+                    case "LOAD_GLOBAL":
+                        types_stacks[-1].append(
+                            Type.from_builtin(
+                                BuiltinTypesEnum.FUNCTION,
+                                builtin_functions.get(inst.argval, Type.from_builtin(BuiltinTypesEnum.UNKNOWN))
+                            )
+                        ) # TODO: currently support typing only builtin functions, other cases cause undefined behaviour
+                        f.write(f"    lea rax, {code_obj.co_names[inst.arg//2]}\n")
+                        f.write("    push rax\n")
+                    case "LOAD_FAST":
+                        f.write(f"    push qword [rbp-8*{inst.arg+1}]\n")
+                        types_stacks[-1].append(variables_types[inst.argval])
+                    case "BINARY_OP":
+                        f.write("    mov rax, [rsp+8]\n")
+                        match inst.argrepr:
+                            case "+"|"-"|"+="|"-="|"&":
+                                f.write(f"    {bin_op_to_inst[inst.argrepr]} rax, [rsp]\n")
+                            case "*":
+                                f.write("    imul rax, [rsp]\n")
+                            case "/":
+                                f.write("    cqo\n")
+                                f.write("    mov rcx, [rsp]\n")
+                                f.write("    idiv rcx\n")
+                            case _:
+                                raise Exception(f"Instruction {inst.opname}({inst.argrepr}) is not implemented")
+                        f.write("    add rsp, 16\n")
+                        types_stacks[-1].pop()
+                        types_stacks[-1].pop()
+                        f.write("    push rax\n")
+                        types_stacks[-1].append(Type.from_builtin(BuiltinTypesEnum.INT))
+                    case "CALL":
+                        for i in range(inst.arg):
+                            f.write(f"    pop {args_to_regs_map[inst.argval-i-1]}\n")
+                        f.write("    pop rax\n")
+                        f.write("    call rax\n")
+                        f.write("    push rax\n")
+                        for i in range(inst.arg):
+                            types_stacks[-1].pop()
+                        types_stacks[-1].append(types_stacks[-1].pop().payload)
+                    case "POP_TOP":
+                        f.write("    add rsp, 8\n")
+                        types_stacks[-1].pop()
+                    case "RETURN_CONST":
+                        f.write(f"    add rsp, {(len(code_obj.co_varnames))*8}\n")
+                        f.write("    pop rbp\n")
+                        f.write(f"    mov rax, {0 if inst.argval is None else inst.argval}\n")
+                        f.write("    ret\n")
+                    case "COMPARE_OP":
+                        f.write("    mov rax, [rsp+8]\n")
+                        f.write("    cmp rax, [rsp]\n")
+                        f.write("    mov rax, 0\n")
+                        f.write(f"    set{cmp_op_to_suffix[inst.argval]} al\n")
+                        f.write("    add rsp, 16\n")
+                        types_stacks[-1].pop()
+                        types_stacks[-1].pop()
+                        f.write("    push rax\n")
+                        types_stacks[-1].append(Type.from_builtin(BuiltinTypesEnum.INT))
+                    case "POP_JUMP_IF_FALSE":
+                        f.write("    pop rax\n")
+                        types_stacks[-1].pop()
+                        f.write("    test rax, rax\n")
+                        f.write(f"    jz {jump_labels[inst.argval]}\n")
+                    case "JUMP_BACKWARD":
+                        f.write(f"    jmp {jump_labels[inst.argval]}\n")
+                    case "JUMP_FORWARD":
+                        f.write(f"    jmp {jump_labels[inst.argval]}\n")
+                    case "PUSH_NULL":
+                        pass
+                        # f.write("    push qword 0\n")
+                    case "NOP":
+                        pass
+                    case _:
+                        raise Exception(f"Instruction {inst.opname}({inst.argrepr}) is not implemented")
+            except Exception as e:
+                raise Exception(f"Compilation error during translating instruction {inst.offset} {inst.opname} ({inst.argrepr})")
         f.seek(0)
         self.codes_asm[code_obj.co_name] = f.read()
 
@@ -193,15 +268,3 @@ if __name__ == "__main__":
 
     compiler = Compiler("main.py", "main.asm")
     compiler.compile_file()
-
-"""
-
-f.write(f"; {inst.offset} MAKE_FUNCTION\n")
-f.write(f"global {inst.argrepr}\n")
-f.write(f"{inst.argrepr}:\n")
-
-f.write("    push rbp\n")
-f.write("    mov rbp, rsp\n")
-f.write(f"    sub rsp, {len(code_obj.co_varnames)*8}\n")
-
-"""
--- main.c
+++ main.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+extern uint64_t None;
 
 void print(int x) {
     printf("%d", x);
@@ -8,6 +12,6 @@ void print_int(int x) {
     printf("%d", x);
 }
 
-void print_string(char* x) {
+void print_string(char *x) {
     printf("%s", x);
 }
